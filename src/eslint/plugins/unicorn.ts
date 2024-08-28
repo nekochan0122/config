287bcf777f4a989987bc1c97d1423142e45c1a0d
NekoChan <aaron74327@gmail.com>
2024-08-28 02:26:12 -0700

    disable no-null, prefer-export-from, prefer-module, prevent-abbreviations

--- src/eslint/plugins/unicorn.ts
+++ src/eslint/plugins/unicorn.ts
@@ -4,4 +4,12 @@ import type { FlatConfigArray } from '~/eslint/types'
 
 export const unicornPluginConfig: FlatConfigArray = [
   eslintPluginUnicorn.configs['flat/recommended'],
+  {
+    rules: {
+      'unicorn/no-null': 'off',
+      'unicorn/prefer-export-from': 'off',
+      'unicorn/prefer-module': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+  },
 ]
