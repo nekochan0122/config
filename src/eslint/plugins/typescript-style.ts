cb54438d1411cd0e6c67d6243688d7c2777d236a
NekoChan <aaron74327@gmail.com>
2024-08-27 23:50:30 -0700

    refactor

--- src/eslint/plugins/typescript-style.ts
+++ src/eslint/plugins/typescript-style.ts
@@ -1,28 +1,34 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
 import type { FlatConfigArray } from '~/eslint/types'
 
-export const typescriptStylePluginConfig: FlatConfigArray = [{
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/array-type': ['error', { default: 'generic' }],
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'separate-type-imports',
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        args: 'all',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        ignoreRestSiblings: true,
-      },
-    ],
-  },
-}]
+export const typescriptStylePluginConfig: FlatConfigArray = [
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic, {
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/array-type': ['error', { default: 'generic' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  }]
--- src/eslint/presets/react.ts
+++ src/eslint/presets/react.ts
@@ -1,6 +1,3 @@
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-
 import { importPluginConfig } from '~/eslint/plugins/import'
 import { importSortPluignConfig } from '~/eslint/plugins/import-sort'
 import { stylisticReactPluginConfig } from '~/eslint/plugins/stylistic-react'
@@ -9,12 +6,9 @@ import { unicornPluginConfig } from '~/eslint/plugins/unicorn'
 import type { FlatConfigArray } from '~/eslint/types'
 
 export const reactPresetConfig: FlatConfigArray = [
-  eslint.configs.recommended,
-  ...tseslint.configs.strict,
-  ...tseslint.configs.stylistic,
-  ...importPluginConfig,
-  ...importSortPluignConfig,
-  ...stylisticReactPluginConfig,
   ...typescriptStylePluginConfig,
+  ...stylisticReactPluginConfig,
+  ...importSortPluignConfig,
+  ...importPluginConfig,
   ...unicornPluginConfig,
 ]
