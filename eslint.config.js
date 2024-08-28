0b769b0da9ca9ed7e306d8dea3e27ac5c9c12834
NekoChan <aaron74327@gmail.com>
2024-08-27 21:03:26 -0700

    fix path

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import tseslint from 'typescript-eslint'
 
-import nekoConfig from './dist/eslint'
+import nekoConfig from './dist/eslint/index.js'
 
 export default tseslint.config(
   ...nekoConfig.presets.base,
