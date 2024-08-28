705589f3b93c1dec692f89a7fef843011d93fe70
NekoChan <aaron74327@gmail.com>
2024-08-28 01:18:53 -0700

    fix fix react-hooks

--- src/eslint/plugins/react-hooks.ts
+++ src/eslint/plugins/react-hooks.ts
@@ -1,3 +1,4 @@
+import { fixupPluginRules } from '@eslint/compat'
 // @ts-expect-error eslint-plugin-react-hooks is not typed
 import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 
@@ -6,8 +7,8 @@ import type { FlatConfigArray } from '~/eslint/types'
 export const reactHooksPluginConfig: FlatConfigArray = [
   {
     plugins: {
-      // https://github.com/facebook/react/issues/28313
-      'react-hooks': eslintPluginReactHooks,
+      // https://github.com/facebook/react/issues/28313#issuecomment-2180984628
+      'react-hooks': fixupPluginRules(eslintPluginReactHooks),
     },
     rules: eslintPluginReactHooks.configs.recommended.rules,
   },
