e47af5ee5553108bd992ea19bd1c8946c749cb53
NekoChan <aaron74327@gmail.com>
2024-08-27 22:19:40 -0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 import nekoConfig from './dist/eslint/index.js'
 
 export default tseslint.config(
   ...nekoConfig.presets.base,
+  {
+    languageOptions: {
+      globals: globals.builtin,
+    },
+  },
+  {
+    ignores: ['dist'],
+  },
 )
