3e5fddb88ec73192b9ade721d12bf963ae1866bc
NekoChan <aaron74327@gmail.com>
2024-08-28 00:50:20 -0700

    fix stylistic-react

--- src/eslint/plugins/stylistic-react.ts
+++ src/eslint/plugins/stylistic-react.ts
@@ -2,14 +2,15 @@ import type { FlatConfigArray } from '~/eslint/types'
 
 import { stylisticBasePluginConfig } from './stylistic-base'
 
-export const stylisticReactPluginConfig: FlatConfigArray = [{
+export const stylisticReactPluginConfig: FlatConfigArray = [
   ...stylisticBasePluginConfig,
-  rules: {
-    '@stylistic/jsx-closing-bracket-location': [1, 'tag-aligned'],
-    '@stylistic/jsx-equals-spacing': [2, 'never'],
-    '@stylistic/jsx-quotes': ['error', 'prefer-single'],
-    '@stylistic/jsx-self-closing-comp': 'error',
-    '@stylistic/jsx-one-expression-per-line': 'off',
-    '@stylistic/jsx-tag-spacing': 'error',
-  },
-}]
+  {
+    rules: {
+      '@stylistic/jsx-closing-bracket-location': [1, 'tag-aligned'],
+      '@stylistic/jsx-equals-spacing': [2, 'never'],
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/jsx-self-closing-comp': 'error',
+      '@stylistic/jsx-one-expression-per-line': 'off',
+      '@stylistic/jsx-tag-spacing': 'error',
+    },
+  }]
